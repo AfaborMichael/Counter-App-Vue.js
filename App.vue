a4c7e6fd06622b162a0ee214c47d35d6e9f85113
Afabor Michael Onafome <fabiomike4@gmail.com>
2023-03-22 22:17:17 +0100

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <NavBar />
+    <RouterView />
+  </div>
+</template>
+<script>
+import NavBar from './components/NavBar.vue'
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+  },
+}
+</script>
+<style scoped>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ Readme.md
@@ -0,0 +1,1 @@
+https://counter-app-vue-js.vercel.app/
